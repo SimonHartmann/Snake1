c7da2736bd150721b69030794b2517f45aca8a18
angelikatr <angelika.trep@gmail.com>
2017-03-07 10:17:12 +0100

    SGKNDSFIUSDFJdf

--- WinSnake/WinSnake/frmSnake.cs
+++ WinSnake/WinSnake/frmSnake.cs
@@ -18,13 +18,19 @@ namespace WinSnake
         //int intKastenY;
         int intBoxSize = 15;
         int intTmerCounter;
+        int intRichtung;
+        int intAnfangX;
+        int intAnfangY;
+        int intEndeX;
+        int intEndeY;
+        int intX, intY;
 
-    
         public frmSnake()
         {
             InitializeComponent();
             sp = new Spiel();
             git = new Gitter();
+           
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -35,22 +41,64 @@ namespace WinSnake
         private void button1_Click(object sender, EventArgs e)
         {
             timerTick.Enabled = true;
-            sp.EssenGenerieren(pbSpielfeld, Brushes.Green, intBoxSize);
+            
             git.Linien(pbSpielfeld);
 
         }
 
         private void pbSpielfeld_Click(object sender, EventArgs e)
         {
-            
 
+            sp.EssenGenerieren(pbSpielfeld, Brushes.Green, intBoxSize);
         }
 
         private void timerTick_Tick(object sender, EventArgs e)
         {
-            sp.Schlange(pbSpielfeld, Brushes.Pink, 1, intTmerCounter, intBoxSize);
+            
+            if (intRichtung == 1) //Links
+            {
+                sp.Schlange(pbSpielfeld, Brushes.Pink, intX--, intY, intBoxSize);
+            }
+            else if (intRichtung == 2) //Rechts
+            {
+                sp.Schlange(pbSpielfeld, Brushes.Pink, intX++, intY, intBoxSize);
+            }
+            else if (intRichtung == 3) //Hoch
+            {
+                sp.Schlange(pbSpielfeld, Brushes.Pink, intX, intY --, intBoxSize);
+            }
+            else if (intRichtung == 4) //Runter
+            {
+                sp.Schlange(pbSpielfeld, Brushes.Pink, intX, intY ++, intBoxSize);
+            }
+            else
+            {
+                sp.Schlange(pbSpielfeld, Brushes.Pink, 16, 15+intTmerCounter, intBoxSize);
+            }
+           
             intTmerCounter++;
 
+        }
+
+        private void frmSnake_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    intRichtung = 1;
+                    break;
+                case Keys.Right:
+                    intRichtung = 2;
+                    break;
+                case Keys.Up:
+                    intRichtung = 3;
+                    break;
+                case Keys.Down:
+                    intRichtung = 4;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
